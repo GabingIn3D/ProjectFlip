2bb087abb35ffdf0d66d32c7e89d242b39d4f471
iannuccills@gmail.com <>
2023-11-21 01:43:00 -0500

    Added comments to the generic comment script

--- ProjectFlipRazr_main/Assets/Scripts/Stephen Stuff/SocialMedia/TextDisplayScript.cs
+++ ProjectFlipRazr_main/Assets/Scripts/Stephen Stuff/SocialMedia/TextDisplayScript.cs
@@ -11,16 +11,19 @@ using UnityEditor.PackageManager.Requests;
 public class TextDisplayScript : MonoBehaviour
 {
     //public string textValue;
+
+    //A reference to every comment box
     public Text comment1;
     public Text comment2;
     public Text comment3;
 
+    //Specifically the generic comment list
     public List<string> genericComments = new List<string>();
     //public GameObject comment;
     //public GameObject commentBox;
 
     
-    public void Awake()
+    public void Awake()//Generates the generic comment list before the first frame
     {
         genericComments.Add("I'm Testing");
         genericComments.Add("Please Work");
@@ -37,19 +40,19 @@ public class TextDisplayScript : MonoBehaviour
 
     public void DisplayMessage()
     {
-        List<int> usedIndexes = new List<int>();
+        List<int> usedIndexes = new List<int>();//Keeps track of what comments were used from the comment list
 
         for (int i = 0; i < 3; i++)
         {
-            int randomNumber;
+            int randomNumber; 
             do
             {
-                randomNumber = Random.Range(0, genericComments.Count);
+                randomNumber = Random.Range(0, genericComments.Count);//Picks a random index number from 0 to whatever amount of generic comments there is
             } while (usedIndexes.Contains(randomNumber));
 
-            usedIndexes.Add(randomNumber);
+            usedIndexes.Add(randomNumber);//Adds the random number just selected to the used list
 
-            switch (i)
+            switch (i)//This is to switch the index if it has been used and pick a new one from the generic comment list
             {
                 case 0:
                     comment1.text = genericComments[randomNumber]; 
@@ -75,12 +78,12 @@ public class TextDisplayScript : MonoBehaviour
         //comment3.text = genericComments[randomNumber];
     }
 
-    const int maxComments = 4;
+    //const int maxComments = 4;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space))//Displays the messages when pressing space
         {
             DisplayMessage();
         }
