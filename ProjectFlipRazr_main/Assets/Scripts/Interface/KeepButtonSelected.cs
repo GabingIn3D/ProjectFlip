96783ee679f87c1b7e2c26a8d11a5ffe46fec156
gabe.d.williams@gmail.com <gabe.d.williams@gmail.com>
2023-12-14 16:42:25 -0500

    latest updates with Gabe's PhotoInfoContainer.cs included

---
+++ ProjectFlipRazr_main/Assets/Scripts/Interface/KeepButtonSelected.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class KeepButtonSelection : MonoBehaviour
+{
+
+    //This script can be attached somewhere in a UI element to make sure that buttons set up in a Navigation group, i.e. Grid Layout Group -- do not lose focus when the player clicks outside of them.
+    //It will automatically select the 'lastSelectedButton' (which it finds on its own in void Update) when a button is deselected
+    public Button defaultButton;
+
+    private GameObject lastSelectedButton;
+    [SerializeField] private GameObject layoutParent;
+    public bool isGallery;
+
+    void OnEnable()
+    {
+        // Set the default button as selected initially
+        if (isGallery)
+        {
+            defaultButton = layoutParent.transform.GetChild(0).GetComponent<Button>();
+        }
+        SetSelectedButton(defaultButton.gameObject);
+    }
+
+    private void OnDisable()
+    {
+        if (isGallery)
+        {
+            defaultButton = null;
+            lastSelectedButton = null;
+        }
+    }
+    void Update()
+    {
+        // Check if no button is currently selected
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            // Re-select the last selected button
+            SetSelectedButton(lastSelectedButton);
+        }
+        else
+        {
+            // Update the last selected button
+            lastSelectedButton = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+
+    void SetSelectedButton(GameObject button)
+    {
+        EventSystem.current.SetSelectedGameObject(button);
+    }
+}
